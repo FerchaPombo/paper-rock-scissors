11ac9f81efdfc7d6ceb9c1ff06374b61d26080d7
FerchaPombo <>
2023-06-06 11:15:58 +0000

    add constants to the Java file based on the code institute template

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+/**
+ *  Constants declared for DOM elements and possible choices
+ */
+// Constants declaration code below based on Code Institute Rock Paper Scissors game
+const buttons = getElementByClassName('control');
+const playerScores = getElementById("player - scores");
+const computerScores = getElementById("computer-scores");
+const playerImage = getElementByID("player-image");
+const computerImage = getElementByID("computer-image");
+const messages = getElementByID('messages');
+const choices = ['rock', 'paper', 'scissors'];
+
